8c27f4f37801e233972683e2257fa4d312590acb
“Ganextred“ <ilyarotan@gmail.comcd c/temp$git config --global user.email ilyarotan@gmail.com>
2022-01-20 16:04:21 +0200

    First Commit

---
+++ src/main/java/com/my/springcinema/Main/SpringCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.my.springcinema.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCinemaApplication.class, args);
+    }
+
+}
