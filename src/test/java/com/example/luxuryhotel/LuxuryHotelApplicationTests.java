06efb95cbf0b2edbb45998b57dcab873d3f52b8d
“Ganextred“ <ilyarotan@gmail.comcd c/temp$git config --global user.email ilyarotan@gmail.com>
2022-01-30 20:41:16 +0200

    First Commit

---
+++ src/test/java/com/example/luxuryhotel/LuxuryHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luxuryhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxuryHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
