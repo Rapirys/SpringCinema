2dbf3a28c405adf050d9df5adb544fea34f978ab
“Ganextred“ <ilyarotan@gmail.comcd c/temp$git config --global user.email ilyarotan@gmail.com>
2022-02-16 17:09:57 +0200

    TestsBugFix

--- src/test/java/com/example/cinema/model/service/HallTopologyTest.java
+++ src/test/java/com/example/cinema/model/service/HallTopologyTest.java
@@ -32,12 +32,13 @@ class HallTopologyTest {
     void GetCopyTopologyByTickets() {
         List<Ticket> tickets =new ArrayList<>();
 
-        for (int i=0; i<hallTopology.topology.size()-5; i+=2){
-            for (int j=0; j<hallTopology.topology.get(i).size()-5; j+=2) {
+        for (int i=0; i<hallTopology.topology.size(); i+=2){
+            for (int j=0; j<hallTopology.topology.get(i).size(); j+=2) {
                 if (hallTopology.topology.get(i).get(j).type.equals('#')) {
                     Ticket ticket = new Ticket();
-                    ticket.setRow(i);
-                    ticket.setPlace(j);
+                    Place p=hallTopology.topology.get(i).get(j);
+                    ticket.setRow(p.row);
+                    ticket.setPlace(p.place);
                     tickets.add(ticket);
                 }
             }
@@ -45,8 +46,9 @@ class HallTopologyTest {
         ArrayList<ArrayList<Place>>  topology= hallTopology.getCopyTopology(tickets);
         for (int i=0; i<hallTopology.topology.size(); i++){
             for (int j=0; j<hallTopology.topology.get(i).size(); j++) {
-                if (i%2==0 && j%2==0 && hallTopology.topology.get(i).get(j).type.equals('#'))
-                    assertThat(topology.get(i).get(j)).isEqualTo('X');
+                if (i%2==0 && j%2==0 && hallTopology.topology.get(i).get(j).type.equals('#')) {
+                    assertThat(topology.get(i).get(j).type).isEqualTo('X');
+                }
                 else  assertThat(topology.get(i).get(j)).isEqualTo(hallTopology.topology.get(i).get(j));
             }
         }
